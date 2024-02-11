6af7ce138a8bccf090591fb1141fff72af32cc7a
Freddy Martínez <fhmartinezq@gmail.com>
2024-02-11 12:56:45 +0100

    feat: get outdated dependecies using npm command

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello from scout cli');
+import { asyncExec } from "./util/asyncExec";
+
+async function getOutdatedPackages() {
+  try {
+    const packages = await asyncExec("npm outdated -l --depth=0 --json");
+    const outdatedPackages = JSON.parse(packages);
+    const packagesToUpdate = Object.keys(outdatedPackages);
+    console.log(packagesToUpdate);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getOutdatedPackages();
---
+++ src/util/asyncExec.ts
@@ -0,0 +1,14 @@
+import { exec } from "child_process"; 
+
+export function asyncExec(command: string) {
+  return new Promise<string>((resolve, reject) => {
+    exec(command, (_, stdout, stderr) => {
+      if (stderr !== "") {
+        reject(stderr);
+      }
+      resolve(stdout);
+    });
+  });
+};
+
+
